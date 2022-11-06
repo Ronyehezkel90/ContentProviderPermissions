ff542566c0ab9535c11f8d0fe6622a23df277810
ronyehezkel <ronyehezkel90@gmail.com>
2022-11-06 18:43:42 +0200

    clean Master

--- app/src/main/java/com/ronyehezkel/contentproviderpermissions/MainActivity.kt
+++ app/src/main/java/com/ronyehezkel/contentproviderpermissions/MainActivity.kt
@@ -23,63 +23,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    @SuppressLint("Range")
-    private fun getAllContacts() {
-
-
-        val editText = findViewById<EditText>(R.id.edit_text)
-        val nameQuery = editText.text.toString()
-        val selection = "${ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME} LIKE ?"
-        val selectionArgs = arrayOf("%$nameQuery%")
-        val phonesCursor = contentResolver.query(
-            CONTENT_URI,
-            null,
-            selection,
-            selectionArgs,
-            null
-        )
-        while (phonesCursor!!.moveToNext()) {
-            val name =
-                phonesCursor.getString(phonesCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
-            Log.d("lesson", name)
-        }
-
-
-    }
-
-    private val requestPermission =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-            if (isGranted) {
-                Log.d("Permissions", "Granted")
-                getAllContacts()
-            } else {
-                Log.d("Permissions", "Not Granted")
-            }
-        }
-
-    private fun validatePermissionAndGetContacts() {
-        val isPermissionAlreadyGranted = ContextCompat.checkSelfPermission(this, READ_CONTACTS)
-        if (isPermissionAlreadyGranted == PackageManager.PERMISSION_GRANTED) {
-            getAllContacts()
-        } else {
-            val needToExplainThePermission =
-                ActivityCompat.shouldShowRequestPermissionRationale(this, READ_CONTACTS)
-            if (needToExplainThePermission) {
-                Snackbar.make(
-                    (findViewById<View>(R.id.button).rootView),
-                    "Hey permission is required in order to find the contacts in your contacts list.",
-                    Snackbar.LENGTH_LONG
-                ).setAction("ASK") {
-                    requestPermission.launch(Manifest.permission.READ_CONTACTS)
-                }.show()
-            } else {
-                requestPermission.launch(Manifest.permission.READ_CONTACTS)
-            }
-        }
-    }
-
     fun getContactsOnClick(view: View) {
-        validatePermissionAndGetContacts()
-//        getAllContacts()
     }
 }
